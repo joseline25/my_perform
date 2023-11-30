fa1738a357e4596356c9992823dce594b5930f85
a-che613 <alexche613@gmail.com>
2023-11-30 14:48:06 +0100

    done with basic styling

--- objective/form.py
+++ objective/form.py
@@ -5,7 +5,27 @@ class ObjectiveForm(forms.ModelForm):
    class Meta:
       model = Objective
       #exclude = ['created_at', 'updated_at']
-      fields = '__all__'
+      # fields = '__all__'
+      fields = [
+            'assign_to',
+            'created_by',
+            'associated_task',
+            'evaluator',
+            'repeat_date',
+            'action_phrase',
+            'number',
+            'units',
+            'start_date',
+            'end_date',
+            'priority',
+            'complexity',
+            'objective_type',
+            'skills',
+            'tools',
+            'dog',
+            'is_draft',
+            'repeat',
+        ]
       
       widgets = {
             'repeat_date': forms.DateTimeInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'placeholder': 'Select a '
@@ -17,6 +37,17 @@ class ObjectiveForm(forms.ModelForm):
             'end_date': forms.DateTimeInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'placeholder': 'Select a '
                                                                                                             'date',
                                                                     'type': 'date'}),
-            
+
+           'action_phrase': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'E.g Clean, Make'}), 
+           'number': forms.NumberInput(attrs={'class': 'form-control'}),
+           'units': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'E.g Email, Notes'}),
+           'priority': forms.Select(attrs={'class': 'form-control'}),
+           'complexity': forms.Select(attrs={'class': 'form-control'}),
+           'dog': forms.CheckboxSelectMultiple(),
+           'assign_to': forms.CheckboxSelectMultiple(),
+           'tools': forms.CheckboxSelectMultiple(),
+           'associated_task': forms.CheckboxSelectMultiple(),
+           'evaluator': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Set who should evaluate this'}),
+           'skills': forms.CheckboxSelectMultiple(),
         }
  
