7f4d1fc05deaedaa7f46835f54bcacdc87c27a01
erickandongo <danielle.ntsamaa@gmail.com>
2023-12-13 14:27:59 +0100

    This is my models

---
+++ action/form.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import *
+
+# validators
+# when number has a value answer is null, when answer has a value number is null
+
--- action/models.py
+++ action/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from objective.models import Objective
+
+
+class Actions(models.Model):
+    action_id = models.AutoField(primary_key=True)
+    objective = models.ForeignKey(Objective, on_delete=models.PROTECT)
+    questions_id = models.ForeignKey(ActionQuestion, on_delete=models.PROTECT)
+    completion_time = models.TimeField(null=False)
+    collaborators_id = models.ForeignKey(Collaborators, on_delete=models.PROTECT)
+    comment = models.TextField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.objective.objective_name} related to {self.action_id}"
+
+class Achievement(models.Model):
+    achieve_id = models.AutoField(primary_key=True)
+    achievement_name = models.CharField(max_length=200)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering=['-date_added']
+
+    def __str__(self):
+        return self.achievement_name
+
+class ActionAchievement(models.Model):
+    achievement_id = models.ForeignKey(Achievement, on_delete=models.PROTECT)
+    action_id = models.ForeignKey(Actions, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.achievement_id.achievement_name
+
 
 # Create your models here.
