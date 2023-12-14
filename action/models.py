802a1a1c9ca063ebb43b79d3abe134def55b5788
FrancineMADOH <francinemadoh@gmail.com>
2023-12-14 12:26:33 +0100

    updated the models

--- action/models.py
+++ action/models.py
@@ -1,25 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import User
-from objective.models import Objective, Tool
+from django.contrib.auth.models import User 
+from objective.models import Objective, Tool,Skill
+from models_additionals.question import Question
 
-# Anderson
-class Question(models.Model):
-    question = models.TextField(null=True, blank=True)
-    number = models.IntegerField(null=True,)
-    answer = models.BooleanField(null=True)
-    related_action = models.ForeignKey(Actions, on_delete=models.CASCADE)
+
+class Actions(models.Model):
+    action_id = models.AutoField(primary_key=True)
+    objective = models.ForeignKey(Objective, on_delete=models.PROTECT)
+    questions_id = models.ForeignKey(Question, on_delete=models.PROTECT)
+    completion_time = models.TimeField(null=False)
+    collaborators = models.ManyToManyField(User,related_name="actions", through="User" ,on_delete=models.PROTECT)
+    comment = models.TextField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, on_delete=models.PROTECT)
     
-    def __str__(self):
-        return self.question
 
-class Collaborator(models.Model):
-    collaborator_id = models.ForeignKey(User, on_delete=models.CASCADE)    
-    created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.objective.objective_name} related to {self.action_id}"
+
+
+
 
 class ActionTool(models.Model):
     tool_id = models.ForeignKey(Tool, on_delete=models.CASCADE)
+    related_action = models.ForeignKey(Actions, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
     
@@ -28,27 +35,12 @@ class ActionTool(models.Model):
     
     
 class ActionSkill(models.Model):
-    skill_id = models.ForeignKey()
-    action_id = models.ForeignKey(Actions, on_delete=models.SET_NULL)
+    skill_id = models.ForeignKey(Skill,on_delete=models.PROTECT)
+    action_id = models.ForeignKey(Actions,on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE) 
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT) 
 
-# Ericka
-class Actions(models.Model):
-    action_id = models.AutoField(primary_key=True)
-    objective = models.ForeignKey(Objective, on_delete=models.PROTECT)
-    questions_id = models.ForeignKey(Question, on_delete=models.PROTECT)
-    completion_time = models.TimeField(null=False)
-    collaborators_id = models.ForeignKey(Collaborator, on_delete=models.PROTECT)
-    comment = models.TextField(max_length=200, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    added_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
-    class Meta:
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f"{self.objective.objective_name} related to {self.action_id}"
 
 class Achievement(models.Model):
     achieve_id = models.AutoField(primary_key=True)
