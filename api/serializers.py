c72b7c6eb58f3a9c6f35c5c44331cd4471f16ba3
joseline25 <youjoseline@gmail.com>
2024-01-26 14:49:36 +0100

    late serializers modifications

--- api/serializers.py
+++ api/serializers.py
@@ -1,39 +1,75 @@
 from rest_framework import serializers
-from objective.models import Objective, Team, KPI
+from objective.models import Objective, Team, KPI, Skill, Tool
+from objective.models_additional.task import Task
 from action.models import Action
 from django.contrib.auth.models import User
 
-# Objective
-class ObjectiveSerializer(serializers.ModelSerializer):
-    class  Meta:
-        model = Objective
+
+# User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+# Skill
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
         fields = '__all__'
 
 
 # Team
 class TeamSerializer(serializers.ModelSerializer):
-    class  Meta:
+    class Meta:
         model = Team
+        fields = '__all__'
+
+# Tool
+class ToolSerializer(serializers.ModelSerializer):
+    teams =TeamSerializer(many=True)
+    class Meta:
+        model = Tool
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
         fields = '__all__'
         
         
-# Action       
+# Objective
+
+
+class ObjectiveSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True)
+    tools = ToolSerializer(many=True)
+    assign_to = UserSerializer(many=True)
+    visible_to = UserSerializer(many=True)
+    associated_task = TaskSerializer(many=True)
+
+    class Meta:
+        model = Objective
+        fields = '__all__'
+
+
+
+
+
+# Action
 class ActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Action
         fields = '__all__'
-        
-        
-# User
-class UserSerializer(serializers.ModelSerializer):
-    class  Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-        
+
 
 # KPI
 
 class KPISerializer(serializers.ModelSerializer):
-    class  Meta:
+    objective = ObjectiveSerializer()
+    class Meta:
         model = KPI
         fields = '__all__'
