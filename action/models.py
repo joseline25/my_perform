b09c4890371c4e8d6a5bfd3c3b6a45d35e433bd9
joseline25 <youjoseline@gmail.com>
2024-02-04 21:38:49 +0100

    correction of serializers for get and post requests

--- action/models.py
+++ action/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from objective.models import Objective, Tool, Skill
-from objective.models import Tool, Skill
 
 
 class Action(models.Model):
@@ -12,7 +11,8 @@ class Action(models.Model):
     # decimal avec 2 decimal , max = 10, blank=False, null=False
     completion_time = models.IntegerField(null=False)
     # ca va venir du front end en heures pour etre converti en secondes, nous on doit convertir en heures pour envoyer au front end
-    collaborators = models.ManyToManyField(User, related_name="collaborators_action", blank=True)
+    collaborators = models.ManyToManyField(
+        User, related_name="collaborators_action", blank=True)
     comment = models.TextField(max_length=200, null=True, blank=True)
     tools = models.ManyToManyField(
         Tool, through="ActionTool", related_name="tool_actions")
@@ -53,9 +53,6 @@ class ActionSkill(models.Model):
         return f"{self.skill_id.skill_name} for {self.action_id.action_name}"
 
 
-
-
-
 class Question(models.Model):
     question = models.TextField(null=True, blank=True)
     action = models.ForeignKey(
@@ -68,3 +65,19 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question
+
+
+
+
+
+{
+    "action_name": "first action",
+    "objective": 1,
+    "completion_time": 3,
+    "collaborators": [1, 2],
+    "comment": "the first action to test the form of the model",
+    "tools": [],
+    "skills": [],
+    "achievements": "Deliverable",
+    "added_by": 1
+}
